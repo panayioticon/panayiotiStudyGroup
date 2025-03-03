933657e478bc2fa91aa45fe719d8fe59e31a9d3e
Panayioti Constantopoulos <u22571460@up.ac.za>
2025-03-03 15:02:16 +0200

    update

--- panayiotiStudyGroup/Controllers/GroupMemberController.cs
+++ panayiotiStudyGroup/Controllers/GroupMemberController.cs
@@ -16,7 +16,7 @@ namespace panayiotiStudyGroup.Controllers
             member.Add(new Models.GroupMember { StudentNumber = "u22571460", Name = "Panayioti", Surname = "Constantopoulos", EmailAddress = "u22571460@tuks.co.za" });
             member.Add(new Models.GroupMember { StudentNumber = "u22666797", Name = "Tiano", Surname = "Menegazzi", EmailAddress = "u22666797@tuks.co.za" });
             member.Add(new Models.GroupMember { StudentNumber = "u24593240", Name = "Sofia", Surname = "Finlayson", EmailAddress = "u24593240@tuks.co.za" });
-            member.Add(new Models.GroupMember { StudentNumber = "u22571460", Name = "Theodoros", Surname = "Constantopoulos", EmailAddress = "u22571460@tuks.co.za" });
+            member.Add(new Models.GroupMember { StudentNumber = "u24571700", Name = "Theodoros", Surname = "Constantopoulos", EmailAddress = "u24571700@tuks.co.za" });
             member.Add(new Models.GroupMember { StudentNumber = "u24616185", Name = "Lukas", Surname = "Brink", EmailAddress = "u24616185@tuks.co.za" });
 
             return View(member);
